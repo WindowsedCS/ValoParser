9f6b17662e9734ea3a06192500e27e1240b38696
WatchAndyTW <andyediteroffice@gmail.com>
2023-02-01 13:01:29 +0800

    fix version includes \u0000 issue

--- NodeParser/tools/VersionParser.py
+++ NodeParser/tools/VersionParser.py
@@ -24,34 +24,49 @@ def get_valorant_version(path = "{0}/ShooterGame/Binaries/Win64/VALORANT-Win64-S
         file = open('./files/manifest/VALORANT.json')
         manifest = json.load(file)
         resJson["manifestId"] = manifest["Manifest ID"]
+
         # Parse EXE file
         data = exe_file.read()
         pattern = "++Ares-Core+release-".encode("utf-16le")
         patternBuild = "ProductVersion".encode("utf-16le")
-        # VALORANT Branch
+
+        # Formatting Data
         pos = data.find(pattern) + len(pattern)
-        branch = "release-{0}".format(data[pos:pos+16].decode("utf-16le").rstrip("\x00"))
+        fullData = data[pos:pos+100].decode("utf-16le").split("\u0000")
+        parsedData = []
+        for i in range(0, len(fullData)):
+            if fullData[i] != "":
+                parsedData.append(fullData[i])
+
+        # VALORANT Branch
+        branch = "release-{0}".format(parsedData[0].rstrip("\x00"))
         resJson["branch"] = branch
+
+        # Build Date
+        date = parsedData[1].rstrip("\x00").split(" ")
+        month =  date[0]
+        day =  date[1]
+        year =  date[2]
+        buildDate = "{0}-{1}-{2}T00:00:00.000Z".format(year, json1[month], day)
+
         # VALORANT Version
-        pos = data.find(pattern) + len(pattern) + 40
-        version = data[pos:pos+32].decode("utf-16le").rstrip("\x00")
+        if "." in parsedData[2].rstrip("\x00"):
+            version = parsedData[2].rstrip("\x00")
+        else:
+            version = parsedData[3].rstrip("\x00")
         resJson["version"] = version
+
         # Build Version
         pos = data.find(patternBuild) + len(patternBuild) + 2
         buildVersion = data[pos:pos+4].decode("utf-16le").rstrip("\x00")
-        # data[pos:pos+100].decode("utf-16le").rstrip("\x00")
         resJson["buildVersion"] = buildVersion
+
         # Riot Client Version
         riotClientVersion = "{0}-shipping-{1}-{2}".format(branch, buildVersion, version.split(".").pop())
         resJson["riotClientVersion"] = riotClientVersion
+
         # Riot Client Build
         get_riot_client_version()
-        # Build Date
-        pos = data.find(pattern) + len(pattern) + 16
-        month =  data[pos:pos+6].decode("utf-16le").rstrip("\x00")
-        day =  data[pos+8:pos+12].decode("utf-16le").rstrip("\x00")
-        year =  data[pos+14:pos+22].decode("utf-16le").rstrip("\x00")
-        buildDate = "{0}-{1}-{2}T00:00:00.000Z".format(year, json1[month], day)
         resJson["buildDate"] = buildDate.replace(" ", "0")
         save_json()
 
@@ -62,6 +77,7 @@ def get_riot_client_version(path = "{0}/RiotClientServices.exe".format(sys.argv[
         pattern = "FileVersion".encode("utf-16le")
         pos = data.find(pattern) + len(pattern) + 4
         riotClientVersion = data[pos:pos+30].decode("utf-16le").rstrip("\x00")
+        
         # Riot Api Version
         with open(path1, "rb") as exe_file:
             data = exe_file.read()
